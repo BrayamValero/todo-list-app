a061ae47558cbb6a8a4a1e65ba40003b5344ad12
Brayam Valero <bvalerop@gmail.com>
2020-03-27 12:44:37 -0500

    Adding plugins & frameworks.

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSS/Plugins: Bootstrap, Fontawesome -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/fontawesome-all.min.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <div class="container">
+        <h4>Hello World</h4>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis tempora repellendus odit adipisci itaque illo dolores consequuntur ex quibusdam sit?</p>
+    </div>
+
+    <!-- Scripts/Plugins: jQuery, Popper, Bootstrap, Sweetalert2 -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/vue.js"></script>
+    <script src="js/sweetalert2.all.min.js"></script>
+
+    <script>
+
+    // Bootstrap: Enable Tooltips & Popovers
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="popover"]').popover();
+    });
+    
+    </script>
+
+  </body>
+</html>
